ae2799d437416180c39c6a93ac5115e3b88af72a
studyVersion <0314sbadre@e-itaca.es>
2023-02-05 17:16:05 +0100

    swipe disabled from the map fragement, to better manipulate the map

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chatapp;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -38,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager2 viewPager2;
     private FirebaseAuth fireAuth;
     private FirebaseUser firebaseUser;
-    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,15 +52,11 @@ public class MainActivity extends AppCompatActivity {
         fireAuth = FirebaseAuth.getInstance();
         firebaseUser = fireAuth.getCurrentUser();
 
-        logout = findViewById(R.id.logoutbtn);
-        logout.setOnClickListener(v -> {
-            fireAuth.signOut();
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        });
 
         if (firebaseUser == null) {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
         }
+        viewPager2.setUserInputEnabled(true);
     }
 
     private void setupViewPager(ViewPager2 viewPager) {
@@ -74,17 +70,18 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new StatusFragment(), "Status");
         if (firebaseUser == null) {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        }else{
+        } else {
             DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(firebaseUser.getUid());
             userRef.child("userType").addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     String userType = dataSnapshot.getValue(String.class);
                     if (userType != null && userType.equals("premium")) {
-                        adapter.addFragment(new MapsFragment(), "Map");
+                        adapter.addFragment(new MapsFragment(), "ParkPal");
                         adapter.notifyDataSetChanged();
                     }
                 }
+
                 @Override
                 public void onCancelled(DatabaseError databaseError) {
                     // handle the error
@@ -92,11 +89,11 @@ public class MainActivity extends AppCompatActivity {
             });
         }
         viewPager2.setAdapter(adapter);
+        viewPager2.setUserInputEnabled(false);
         new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
             tab.setText(adapter.mFragmentTitleList.get(position));
         }).attach();
     }
-
 
     class ViewPagerAdapter extends FragmentStateAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
@@ -122,11 +119,26 @@ public class MainActivity extends AppCompatActivity {
             return mFragmentList.size();
         }
     }
+    protected void disableSwipe() {
+        viewPager2.setUserInputEnabled(false);
+    }
 
+    protected void enableSwipe() {
+        viewPager2.setUserInputEnabled(true);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem logoutItem = menu.findItem(R.id.logout);
+        logoutItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                fireAuth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                return true;
+            }
+        });
         return true;
     }
 
